b16eef398bd07cd0f92b2f59c9e74111069fd8c9
rakeshnayal-k <rakeshdravid25@gmail.com>
2023-11-20 22:06:11 +0530

    Model serializers

--- user_info/serializers.py
+++ user_info/serializers.py
@@ -12,7 +12,9 @@ class UserInfoSerializer(serializers.Serializer):
 
 class UserAddressSerializer(serializers.ModelSerializer):
     # phone = UserInfoSerializer()
+    pin_code = serializers.CharField(read_only=True) # read_only for a single field 
     class Meta:
         model = UserAddress
         fields = '__all__'
         # fields = ['phone','pin_code','State','district']     
+        # read_only = ['pin_code','state'] # read_only for multiple fields
